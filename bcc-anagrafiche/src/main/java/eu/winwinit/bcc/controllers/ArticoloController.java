5713477e758c25efd281d94c4200f98ce1b1744f
Simone Trovato <trovato@omicronconsulting.it>
2021-01-29 14:20:56 +0100

    correzione

--- bcc-anagrafiche/src/main/java/eu/winwinit/bcc/controllers/ArticoloController.java
+++ bcc-anagrafiche/src/main/java/eu/winwinit/bcc/controllers/ArticoloController.java
@@ -1,5 +1,7 @@
 package eu.winwinit.bcc.controllers;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,43 +16,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 import eu.winwinit.bcc.entities.Articolo;
 import eu.winwinit.bcc.service.ArticoloService;
+import eu.winwinit.bcc.service.OrdineService;
 import javassist.tools.web.BadHttpRequest;
 @RestController
 @RequestMapping(path = "/articolo")
 public class ArticoloController {
 
-   @Autowired
-    private ArticoloService service;
-    
-    @GetMapping(value="read")
-    public Iterable<Articolo> findAll() {
-        return service.getAllArticoli();
-    }
-    
-    @GetMapping(path = "read/{id}")
-    public Articolo find(@PathVariable("id") Integer id) {
-      return service.getArticoloById(id);
-    }
-    
-    @PostMapping(value="/create", consumes = "application/json")
-    public Articolo create(@RequestBody Articolo articolo) {
-        return service.createArticolo(articolo);
-    }
-    
-    @DeleteMapping(path = "/delete/{id}")
-    public void delete(@PathVariable("id") Integer id) {
-    	service.deleteArticolo(id);
-    }
-    
-    @PutMapping(path = "/upload")
-    public ResponseEntity<String> update(@RequestBody Articolo articolo) throws BadHttpRequest {
-        if (service.getArticoloById(articolo.getId()) != null) {  
-        	if(service.updateArticolo(articolo)) {
-        		return new ResponseEntity<>("Articolo aggiornato con successo", HttpStatus.OK);
-        	} else
-        		return new ResponseEntity<>("Impossibile aggiornare l'Articolo con id "+articolo.getId(), HttpStatus.OK);
-        }
-        return new ResponseEntity<>("Articolo non esistente", HttpStatus.BAD_REQUEST);
-    }
-    
+	@Autowired
+	private ArticoloService service;
+	private OrdineService ordServ; 
+
+	@GetMapping(value="read")
+	public Iterable<Articolo> findAll() {
+		return service.getAllArticoli();
+	}
+
+	@GetMapping(path = "read/{id}")
+	public ResponseEntity<?> find(@PathVariable("id") Integer id) {
+		if(service.getArticoloById(id) != null) {
+			return new ResponseEntity<>(service.getArticoloById(id), HttpStatus.OK);
+		}
+		else 
+			return new ResponseEntity<>("Articolo non esistente", HttpStatus.BAD_REQUEST);
+
+
+	}
+
+	@PostMapping(value="/create", consumes = "application/json")
+	public Articolo create(@RequestBody Articolo articolo) {
+		return service.createArticolo(articolo);
+	}
+
+	@DeleteMapping(path = "/delete/{id}")
+	public ResponseEntity<String> delete(@PathVariable("id") Integer id) throws SQLIntegrityConstraintViolationException  {
+		if (service.getArticoloById(id) != null) {  
+			if(service.deleteArticolo(id) ) {
+				return new ResponseEntity<>("Articolo eliminato con successo", HttpStatus.OK);
+			}else
+				return new ResponseEntity<>("Impossibile eliminare l'Articolo con id "+id, HttpStatus.OK);
+		}
+		return new ResponseEntity<>("Articolo non esistente", HttpStatus.BAD_REQUEST);
+	}
+
+	@PutMapping(path = "/update")
+	public ResponseEntity<String> update(@RequestBody Articolo articolo) throws BadHttpRequest {
+		if (service.getArticoloById(articolo.getId()) != null) {  
+			if(service.updateArticolo(articolo)) {
+				return new ResponseEntity<>("Articolo aggiornato con successo", HttpStatus.OK);
+			} else
+				return new ResponseEntity<>("Impossibile aggiornare l'Articolo con id "+articolo.getId(), HttpStatus.OK);
+		}
+		return new ResponseEntity<>("Articolo non esistente", HttpStatus.BAD_REQUEST);
+	}
+
 }
